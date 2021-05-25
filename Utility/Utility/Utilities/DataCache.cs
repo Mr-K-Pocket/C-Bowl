02d36ae6e46d6e3182480211b63b9bcb9ae6dc5d
CrazyyyRamen <itkevinwxy@gmail.com>
2021-05-24 22:01:41 -0400

    Utility Cache

---
+++ Utility/Utility/Utilities/DataCache.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Utilities
+{
+    public class DataCache : MemoryCache
+    {
+        public DataCache() : base("dataCache")
+        { 
+        
+        }
+
+        public static T GetCachedItem<T>(string key) where T : class
+        {
+            try
+            {
+                ObjectCache cache = MemoryCache.Default;
+                return (T)cache[key];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static void SetCachedItem<T>(T obj, string key)
+        {
+            if (obj != null)
+            {
+                ObjectCache cache = MemoryCache.Default;
+                cache.Set(key, obj, GetCachePolicy());
+            }
+        }
+
+        public static CacheItemPolicy GetCachePolicy()
+        {
+            CacheItemPolicy policy = new CacheItemPolicy();
+
+            policy.Priority = CacheItemPriority.Default;
+            policy.SlidingExpiration = TimeSpan.FromHours(2.0);
+
+            return policy;
+        }
+
+        public static void FlushAllCache()
+        {
+            ObjectCache cache = MemoryCache.Default;
+
+            cache.Select(kv => kv.Key).ToList().ForEach(
+                k => cache.Remove(k)    
+            );
+        }
+
+        public static void RemoveCacheByKey(string key)
+        {
+            ObjectCache cache = MemoryCache.Default;
+            cache.Remove(key);
+        }
+    }
+}
